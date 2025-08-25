e599e7db55a9956604af588cc629f1fb0ad64139
JEsteban159 <jestebanasturiaso159@gmail.com>
2025-08-25 00:12:16 -0600

    []

--- Actividad4/src/actividad4/Actividad4.java
+++ Actividad4/src/actividad4/Actividad4.java
@@ -23,7 +23,7 @@ public class Actividad4 {
             
             switch (opcion) {
                 case "uno":
-//                    do {
+                    do {
                         Scanner sumar = new Scanner(System.in);
                         System.out.println("Ingrese el primer número: ");
                         int a=sumar.nextInt();
@@ -35,23 +35,78 @@ public class Actividad4 {
                         System.out.println("Quiere repetir S/N: ");
                         String confirmacion = contenedorEntradas.nextLine();
                         if ("N".equals(confirmacion)) break;
-//                    }while (true);
+                    }while (true);
                 break;
+               
                 case "dos":
-                   System.out.println("Error. Función aún no añadida...");
+                    do {
+                        Scanner restar = new Scanner(System.in);
+                        System.out.println("Ingrese el primer número: ");
+                        int a=restar.nextInt();
+                        System.out.println("Ingrese el segundo número: ");
+                        int b=restar.nextInt();
+                        int resta = a - b;
+                        System.out.println("La resta es: " + resta);
+                        
+                        System.out.println("Quiere repetir S/N: ");
+                        String confirmacion = contenedorEntradas.nextLine();
+                        if ("N".equals(confirmacion)) break;
+                    }while (true);
                 break;
+                
                 case "tres":
-                   System.out.println("Error. Función aún no añadida...");
+                    do {
+                        Scanner multiplicar = new Scanner(System.in);
+                        System.out.println("Ingrese el primer número: ");
+                        int a=multiplicar.nextInt();
+                        System.out.println("Ingrese el segundo número: ");
+                        int b=multiplicar.nextInt();
+                        int multiplicacion = a * b;
+                        System.out.println("La multiplicación es: " + multiplicacion);
+                        
+                        System.out.println("Quiere repetir S/N: ");
+                        String confirmacion = contenedorEntradas.nextLine();
+                        if ("N".equals(confirmacion)) break;
+                    }while (true);
                 break;
+               
                 case "cuatro":
-                   System.out.println("Error. Función aún no añadida...");
+                    do {
+                        Scanner dividir = new Scanner(System.in);
+                        System.out.println("Ingrese el primer número: ");
+                        double a=dividir.nextDouble();
+                        System.out.println("Ingrese el segundo número: ");
+                        double b=dividir.nextDouble();
+                        double division = a / b;
+                        System.out.printf("La división es: %.2f%n", division);
+                        
+                        System.out.println("Quiere repetir S/N: ");
+                        String confirmacion = contenedorEntradas.nextLine();
+                        if ("N".equals(confirmacion)) break;
+                    }while (true);
                 break;
+               
                 case "cinco":
-                   System.out.println("Error. Función aún no añadida...");
+                    do {
+                        System.out.println("Error. Función aún no añadida...");
+                        
+                        System.out.println("Quiere repetir S/N: ");
+                        String confirmacion = contenedorEntradas.nextLine();
+                        if ("N".equals(confirmacion)) break;
+                        if ("n".equals(confirmacion)) break;
+                    }while (true);
                 break;
+               
                 case "seis":
-                   System.out.println("Error. Función aún no añadida...");
+                    do {
+                        System.out.println("Error. Función aún no añadida...");
+                        
+                        System.out.println("Quiere repetir S/N: ");
+                        String confirmacion = contenedorEntradas.nextLine();
+                        if ("N".equals(confirmacion)) break;
+                    }while (true);
                 break;    
+                
                 case "siete":
                     salir = true;
                 break;
